bc79d90df8c72c0dfca6110003a350e5a8fc7a13
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-02-27 17:25:50 +0900

    Update da03_SQL_Advanced.sql

--- day03/da03_SQL_Advanced.sql
+++ day03/da03_SQL_Advanced.sql
@@ -40,6 +40,13 @@ FROM Orders;
 
 SELECT SYSDATE(), DATE_FORMAT(SYSDATE(), '%Y%m%d:%H%i%s');
 
+-- D-DAY
+SELECT DATEDIFF(SYSDATE(), '2025-02-03');
+
+-- Formatting , 1000 단위 마다 , 넣기
+SELECT  bookid, FORMAT(price,0) AS price
+FROM	MyBook;
+
 -- 4-8
 SELECT orderid '주문번호', DATE_FORMAT(orderdate,'%Y-%m-%d') '주문일' , custid '고객번호' , bookid '도서번호'
 FROM Orders
@@ -47,3 +54,24 @@ WHERE orderdate = STR_TO_DATE('20140707','%Y%m%d');
 
 
 -- NULL = Python None 동일. 모든 다른 프로그래밍 언어에서는 전부 NULL, NUL
+-- 추가. 금액이 NULL 일 때 발생되는 현상
+
+SELECT 	price
+FROM	MyBook
+WHERE bookid = 3;
+
+
+-- 핵심. 집계함수가 다 꼬임
+SELECT 	 SUM(price) AS '합산은 문제없음'
+		,AVG(price) AS '평균이 NULL값이 빠져서 꼬임'
+        ,COUNT(price) AS	'NULL이 빠져 2가 됨'
+        ,COUNT(*)	AS '3이나옴'
+FROM	MyBook;
+-- WHERE 	price IS NOT NULL; < NULL 값 피하기
+
+
+-- NULL값 확인. NULL은 비교연산자 사용불가
+-- IS 키워드 사용
+SELECT 	*
+FROM	MyBook
+WHERE 	price IS NOT NULL;
